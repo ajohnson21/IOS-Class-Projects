87d2bd45958a10b308a1855da8945e3c8ebdef33
ajohnson21 <aje221@Austens-MacBook-Air.local>
2014-03-31 11:28:14 -0400

    Create fun app

---
+++ Fun App/Fun App/AJFViewController.h
@@ -0,0 +1,13 @@
+//
+//  AJFViewController.h
+//  Fun App
+//
+//  Created by Austen Johnson on 3/31/14.
+//  Copyright (c) 2014 Austen Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJFViewController : UIViewController
+
+@end
---
+++ Fun App/Fun App/AJFViewController.m
@@ -0,0 +1,29 @@
+//
+//  AJFViewController.m
+//  Fun App
+//
+//  Created by Austen Johnson on 3/31/14.
+//  Copyright (c) 2014 Austen Johnson. All rights reserved.
+//
+
+#import "AJFViewController.h"
+
+@interface AJFViewController ()
+
+@end
+
+@implementation AJFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
