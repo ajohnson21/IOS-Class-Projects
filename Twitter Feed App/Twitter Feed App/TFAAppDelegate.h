4426b465119759fd48849c708754261492b3bcba
ajohnson21 <aje221@Austens-MacBook-Air.local>
2014-04-13 14:40:35 -0400

    added an nsdictionary of 3 chars of  a tweet

---
+++ Twitter Feed App/Twitter Feed App/TFAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TFAAppDelegate.h
+//  Twitter Feed App
+//
+//  Created by Austen Johnson on 4/12/14.
+//  Copyright (c) 2014 Austen Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Twitter Feed App/Twitter Feed App/TFAVC.h
@@ -0,0 +1,13 @@
+//
+//  TFAVC.h
+//  Twitter Feed App
+//
+//  Created by Austen Johnson on 4/12/14.
+//  Copyright (c) 2014 Austen Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFAVC : UITableViewController
+
+@end
