97dde7a7e649b083daf0c62d637ce25f5f3f5d5d
ajohnson21 <aje221@Austens-MacBook-Air.local>
2014-05-30 11:30:23 -0400

    CollectionView Overview

---
+++ GridLayout/GLACollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  GLACollectionViewController.h
+//  GridLayout
+//
+//  Created by Austen Johnson on 5/30/14.
+//  Copyright (c) 2014 Austen Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLACollectionViewController : UICollectionViewController
+
+@end
---
+++ GridLayout/GridLayout/GLAViewController.h
@@ -0,0 +1,13 @@
+//
+//  GLAViewController.h
+//  GridLayout
+//
+//  Created by Austen Johnson on 5/30/14.
+//  Copyright (c) 2014 Austen Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLAViewController : UIViewController
+
+@end
